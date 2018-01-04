6c6d9a063859d03d55b649cf6b40e09040126232
lstincoso <lsalhuatincos.o@gmail.com>
2018-01-04 15:48:15 -0500

    coneccion

--- db_conection.php
+++ db_conection.php
@@ -1,9 +1,9 @@
 <?php
-$dbhost='localhost';
+$dbhost='ec2-54-227-247-225.compute-1.amazonaws.com';
 $dbport='5432';
-$dbuser='postgres';
-$dbpass='1234';
-$database='service_Life';
+$dbuser='rfxaahfriyaito';
+$dbpass='0e5f0bf8e46c67f56ffc655dc1ed540706aa824da3719c2a91875446c4a0f98f';
+$database='dc8nbpujpv270e';
 $dbconnect = pg_connect("host=localhost port=5432 dbname=service_Life user=postgres password=1234")
  or die ("Could not connect");
 
